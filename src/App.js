b10b40a2b70a9187e7a7b9aeeb060dbba519eb5e
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-10-24 14:25:16 +0100

    Basic ease animation with delay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import gsap from 'gsap'
+
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+  useEffect(() => {
+    gsap.to(logoItem, { duration: 0.8, opacity: 1, y: -20, ease: "power3.easeOut" })
+    gsap.to(textItem, { duration: 0.8, opacity: 1, y: -20, ease: "power3.easeOut", delay: 0.2 })
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={el => { logoItem = el }}
+          src={logo} className="App-logo" alt="logo" />
+        <p
+          ref={el => { textItem = el }}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
